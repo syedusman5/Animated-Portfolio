01fb935a839da9f694a9a5ecdf77d70e88b9ddb8
syedusman5 <syedusmannaim@gmail.com>
2025-08-22 22:12:01 +0530

    Initial commit

---
+++ src/sections/hero.jsx
@@ -0,0 +1,12 @@
+import HeroText from "../components/HeroText"
+
+const Hero = () => {
+  return (
+    <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden">
+      <HeroText/>
+      
+    </section>
+  )
+}
+
+export default Hero
